10ca4b9ce8023a6aa67beeca4223e10849f82efc
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2023-11-28 09:24:52 +0100

    Initial Commit

---
+++ BiometricAuthentication/BiometricAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthenticationApp.swift
+//  BiometricAuthentication
+//
+//  Created by Diego Ballesteros on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
